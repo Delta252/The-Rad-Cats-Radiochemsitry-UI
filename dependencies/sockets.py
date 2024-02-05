fa3f29aedae0ac75520df2ca7ba9590a576b7ab6
Stas Irisov <svirisov@gmail.com>
2024-02-05 15:49:38 +0000

    Expanded List of Sockets Functions
    - Expanded list of defined function calls to include those required for basic demo
    - Included functions are tailored to binary operation of syringe pump

--- dependencies/sockets.py
+++ dependencies/sockets.py
@@ -10,3 +10,28 @@ def handle_connect():
 def test_ping():
     print('Ping received!')
     socketio.emit('send_ping', {'data':'Test connection'})
+
+@socketio.on('get-comms-status')
+def get_comms_status():
+    result = comms.isConnected
+    socketio.emit('send_comms_status', data=(result))
+
+@socketio.on('toggle-comms')
+def toggle_comms():
+    connection = comms.isConnected
+    if connection:
+        result = comms.stop()
+    else:
+        result = comms.start()
+    socketio.emit('send_comms_status', data=(result))
+    socketio.emit('toggle_comms', {'data':result})
+
+@socketio.on('pull-syringe')
+def get_comms_status():
+    command = '[sID1000 rID1008 PK3 Y1 S2000 D1]'
+    comms.runCommand(command)
+
+@socketio.on('push-syringe')
+def get_comms_status():
+    command = '[sID1000 rID1008 PK3 Y1 S0 D0]'
+    comms.runCommand(command)
