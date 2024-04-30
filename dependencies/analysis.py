b9ff5f2db8113f9efd2c515fe603dfe7df233d7d
DS\stanislav.irisov <svirisov@gmail.com>
2024-04-30 10:53:54 +0100

    Added Placeholder Analysis File
    - File will be responsible for data plotting sensor readings

---
+++ dependencies/analysis.py
@@ -0,0 +1,8 @@
+# Any data analysis and visualization functions
+# This file handles the visualization of results gathered from the sensors
+# and spectrometer for the user to view
+
+class Analysis:
+    def __init__(self):
+        self.dataSource = ''
+        self.resultDestination = ''
